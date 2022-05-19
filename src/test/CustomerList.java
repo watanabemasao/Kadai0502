b5ce054c6adea899102a06a9a22f401a85a0a88b
渡辺正雄 <watanabemasao@watanabemasaonoMacBook-Pro.local>
2022-05-19 22:31:00 +0900

    課題提出

---
+++ src/test/CustomerList.java
@@ -0,0 +1,22 @@
+package test;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+public class CustomerList {
+    public ArrayList<CustomerForm> customerList() {
+
+        var no1 = new CustomerForm(12, "田中", "男");
+        var no2 = new CustomerForm(13, "山本", "男");
+        var no3 = new CustomerForm(12, "鈴木", "女");
+        var no4 = new CustomerForm(13, "佐藤", "女");
+
+        var no5 = new CustomerForm(-1, "井上", "女");
+        var no6 = new CustomerForm(199, "岡田", "女");
+
+        var no7 = new CustomerForm(12, "山﨑", "あ");
+        var no8 = new CustomerForm(13, "吉本", "あ");
+
+        return new ArrayList<>(Arrays.asList(no1,no2, no3, no4, no5, no6, no7, no8));
+    }
+}
