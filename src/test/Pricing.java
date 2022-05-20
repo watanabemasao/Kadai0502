c869a351e1d7dccbecf9d2b0766a458c8fb07be6
渡辺正雄 <watanabemasao@watanabemasaonoMacBook-Pro.local>
2022-05-21 02:54:04 +0900

    曜日取得に関するコードの記述を変更

--- src/test/Pricing.java
+++ src/test/Pricing.java
@@ -12,30 +12,18 @@ public class Pricing {
     private static final  String sexSet = sexM;
 
     private static final int ageSet = 13;
-    private static final String eventDaySet = "木曜日";
+    private static final String eventDaySet = "土曜日";
 
     private static final int basePriceSet = 2000;
 //----------------------------------------------------------------------//
-    private static String DayJpn() {
+    private static String CallDayWeek() {
+        String[] callDayWeek = {"","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日","日曜日"};
         LocalDate now = LocalDate.now();
         DayOfWeek day = now.getDayOfWeek();
-        String dayJpn = "";
-        if (day == DayOfWeek.MONDAY) {
-            dayJpn = "月曜日";
-        } else if (day == DayOfWeek.TUESDAY) {
-            dayJpn = "火曜日";
-        } else if (day == DayOfWeek.WEDNESDAY) {
-            dayJpn = "水曜日";
-        } else if (day == DayOfWeek.THURSDAY) {
-            dayJpn = "木曜日";
-        } else if (day == DayOfWeek.FRIDAY) {
-            dayJpn = "金曜日";
-        } else if (day == DayOfWeek.SATURDAY) {
-            dayJpn = "土曜日";
-        } else if (day == DayOfWeek.SUNDAY) {
-            dayJpn = "日曜日";
-        } return dayJpn;
+        int i = day.getValue();
+       return callDayWeek[i];
     }
+
 
     public static void info(CustomerForm com) {
 
@@ -53,8 +41,8 @@ public class Pricing {
                 }
             } else if (sexSet.equals(com.getGender())) {
                     System.out.println(com.getName() + "様は本日、" + basePriceSet + "円です。");
-            } else if (eventDaySet.equals(DayJpn())) {
-                    System.out.println(com.getName() + "様は本日、" + DayJpn() + "につき、3割引で" + round(basePriceSet * 0.7) + "円です。");
+            } else if (eventDaySet.equals(CallDayWeek())) {
+                    System.out.println(com.getName() + "様は本日、" + CallDayWeek() + "につき、3割引で" + round(basePriceSet * 0.7) + "円です。");
 
             } else System.out.println(com.getName() + "様は本日、" + basePriceSet + "円です。");
         }else System.out.println("Error 性別の値が正しくありません。（”男”又は”女”で入力してください）");
